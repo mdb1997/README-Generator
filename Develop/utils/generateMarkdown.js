30c8ff82ea84a1c04c2e40757c7f0eaf7ca68eb1
mdb1997 <140769684+mdb1997@users.noreply.github.com>
2023-11-13 19:28:28 -0500

    Created a return link function+changed badge color

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,14 +3,16 @@
 function renderLicenseBadge(license) {
   let badgeLabel = license.replace(" ", "&ensp;");
   return `
-  [![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-green.svg)](${renderLicenseLink(license)})
+  [![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-purple.svg)](${renderLicenseLink(license)})
   `
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+let linkUrl = license.toLowerCase().replace(" ", "-");
+return `https://choosealicense.com/licenses/${linkUrl}/.`
+};
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
