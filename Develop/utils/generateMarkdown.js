98c24765deffaf36fbec8959e073678928e678cb
mdb1997 <140769684+mdb1997@users.noreply.github.com>
2023-11-13 19:36:28 -0500

    Created README license function.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,7 +15,18 @@ return `https://choosealicense.com/licenses/${linkUrl}/.`
 };
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (license === "None") {
+  return "";
+} else {
+
+  return `
+### Licensing 
+${license}
+${renderLicenseBadge(license)}
+`
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
